094ddf86add20095eb4da3a86ce92dfbf7991971
Avinash Baghel <bavinash710@gmail.com>
2026-07-12 11:05:09 +0530

    Update 1_Two_Sum.cpp

--- Array/1_Two_Sum.cpp
+++ Array/1_Two_Sum.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int , int> mpp;
+        for(int i = 0; i < nums.size(); i++){
+            int a = nums[i];
+            int check = target - a;
+            if(mpp.find(check) != mpp.end()){
+                return {mpp[check] , i};
+            }
+            mpp[a] = i;
+        }
+        return {-1 , -1};
+    }
+};
+
 

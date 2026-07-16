df94afb7f1ee6d8f24fabea7f0ddb7d5efccf548
Avinash Baghel <bavinash710@gmail.com>
2026-07-16 22:47:04 +0530

    Create Trapping Rainwater.cpp

---
+++ Stack & Queue/Trapping Rainwater.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int l = 0, r = n - 1;
+        int ans = 0;
+        int leftmax = 0, rightmax = 0;
+
+        while (l < r) {
+            leftmax = max(leftmax, height[l]);
+            rightmax = max(rightmax, height[r]);
+
+            if (leftmax < rightmax) {
+                ans += leftmax - height[l];
+                l++;
+            } else {
+                ans += rightmax - height[r];
+                r--;
+            }
+        }
+
+        return ans;
+    }
+};

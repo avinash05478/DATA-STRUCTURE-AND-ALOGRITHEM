5e8f4f3d3116b92babe4fa2e8c4f03dce6a51d04
Avinash Baghel <bavinash710@gmail.com>
2026-07-18 11:38:42 +0530

    Create Largest rectangle in a histogram.cpp

---
+++ Stack & Queue/Largest rectangle in a histogram.cpp
@@ -0,0 +1,32 @@
+// isme hum area precompute krter rhenge sath ki sath
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int maxarea = 0;
+        int n = heights.size();
+
+        for(int i = 0; i<n; i++){
+            while(!st.empty() && heights[st.top()] > heights[i]){
+                int ele = st.top();
+                st.pop();
+
+                int nse = i;
+                int pse = st.empty() ? -1 : st.top();
+
+                maxarea = max(maxarea , (nse - pse - 1) * heights[ele]);
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()){
+            int nse = n;
+            int ele = st.top();
+            st.pop();
+            int pse = st.empty() ? -1 : st.top();
+
+            maxarea = max(maxarea , (nse - pse - 1) * heights[ele]);
+        }
+        return maxarea;
+    }
+};
